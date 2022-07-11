b375851b74cd0cd20325dae0bdce6fe30d2a1864
Sue <sue.su.0526@gmail.com>
2022-07-10 21:25:39 -0400

    updated projects

--- projects.js
+++ projects.js
@@ -1,82 +1,90 @@
 const projects = [
     {
+        projectName:"Quote Keeper",
         icon: "./images/icon_spring.svg",
-        projectName:"Quote Keeper/My Library",
         additionalInfo: "",
         stack:["Spring Boot", " Spring Security", " Java", " MySQL", " React", " AWS S3"],
         image: "./images/cover-quote-keeper.PNG",
         video:"",
         live:"",
-        code:""
+        code:"",
+        date: "2022-07"
     },
     {
+        projectName:"Family Feud Quiz Game",
         icon: "./images/icon_spring.svg",
-        projectName:"Family Feud Game",
         additionalInfo: "",
         stack:["Spring Boot", " Java", " JavaScript", " Thymeleaf", " HTML", " CSS"],
         image: "./images/cover-family-feud.PNG",
         video:"",
         live:"",
-        code:""
+        code:"",
+        date: "2022-07"
     },
     {
-        icon: "./images/icon_js.svg",
         projectName:"Chinese Checkers",
+        icon: "./images/icon_js.svg",
         additionalInfo: "(2-6 players)",
         stack:["JavaScript", " Socket.IO", " HTML", " CSS"],
         image: "./images/cover-chinese-checker.PNG",
         video:"",
         live:"",
-        code:"https://github.com/ssu526/game_chinese_checker"
+        code:"https://github.com/ssu526/game_chinese_checker",
+        date: "2022-06"
     }, 
     {
-        icon: "./images/icon_react.svg",
         projectName:"Mood Calendar",
+        icon: "./images/icon_react.svg",
         additionalInfo: "",
         stack:["React", " Firebase Realtime Database", " Firbase Authentication", " JavaScript", " HTML", " CSS"],
         image: "./images/cover-mood-tracker.PNG",
-        video:"",
-        live:"",
-        code:"https://github.com/ssu526/mood-calendar"
+        video:"https://www.youtube.com/watch?v=af5zCiPj1pg",
+        live:"https://idyllic-lollipop-6ef2b2.netlify.app",
+        code:"https://github.com/ssu526/mood-calendar",
+        date: "2022-06"
     },
     {
-        icon: "./images/icon_react.svg",
         projectName:"Macbook Notes App Clone",
+        icon: "./images/icon_react.svg",
         additionalInfo: "",
         stack:["React", " JavaScript", " HTML", " CSS"],
         image: "./images/cover-mac-notes.PNG",
-        video:"",
+        video:"https://www.youtube.com/watch?v=_2wV-1hZxA0",
         live:"",
-        code:"https://github.com/ssu526/mac_notes_clone"
+        code:"https://github.com/ssu526/mac_notes_clone",
+        date: "2022-06"
     },
     {
-        icon: "./images/icon_js.svg",
         projectName:"Tetris",
+        icon: "./images/icon_js.svg",
         additionalInfo: "",
         stack:["JavaScript", " HTML", " CSS"],
         image: "./images/cover-tetris.PNG",
-        video:"",
+        video:"https://www.youtube.com/watch?v=jxgFOmgTrP0",
         live:"https://famous-queijadas-e79014.netlify.app/",
-        code:"https://github.com/ssu526/game_tetris"
+        code:"https://github.com/ssu526/game_tetris",
+        date: "2022-06"
     }, 
     {
-        icon: "./images/icon_js.svg",
         projectName:"Sudoku",
+        icon: "./images/icon_js.svg",
         additionalInfo: "",
         stack:["JavaScript", " HTML", " CSS"],
         image: "./images/cover-sudoku.PNG",
         video:"https://www.youtube.com/watch?v=LZPiUMtmgHc",
         live:"https://teal-halva-68bb0a.netlify.app/",
-        code:"https://github.com/ssu526/game_sudoku"
+        code:"https://github.com/ssu526/game_sudoku",
+        date: "2022-06"
     }, 
     {
-        icon: "./images/icon_js.svg",
         projectName:"2048",
+        icon: "./images/icon_js.svg",
         additionalInfo: "",
         stack:["JavaScript", " HTML", " CSS"],
         image: "./images/cover-2048.PNG",
         video:"",
         live:"https://celadon-rabanadas-57983d.netlify.app/",
-        code:"https://github.com/ssu526/game_2048/"
+        code:"https://github.com/ssu526/game_2048/",
+        date: "2022-06"
     },
 ]
